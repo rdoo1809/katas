29660ed9e7d18701307ca48b802f984970a656cd
Ryan Dooley <r1d8o0o9@gmail.com>
2024-07-29 16:45:04 -0230

    refactor

--- TennisGame2.php
+++ TennisGame2.php
@@ -4,7 +4,6 @@ class TennisGame2 implements TennisGame
 {
     private $playerOnePoint = 0;
     private $playerTwoPoint = 0;
-
     private $playerOneResult = "";
     private $playerTwoResult = "";
     private $playerOneName = "";
@@ -18,7 +17,6 @@ class TennisGame2 implements TennisGame
 
     public function getScore()
     {
-        $score = "";
         $scoreIsTied = ($this->playerOnePoint == $this->playerTwoPoint) && $this->playerOnePoint < 4;
         $scoreIsDeuce = $this->playerOnePoint == $this->playerTwoPoint && $this->playerOnePoint >= 3;
         $isPlayerOneAdvantage = $this->playerOnePoint > $this->playerTwoPoint && $this->playerTwoPoint >= 3;
@@ -26,43 +24,37 @@ class TennisGame2 implements TennisGame
         $isPlayerOneWin = $this->playerOnePoint >= 4 && $this->playerTwoPoint >= 0 && ($this->playerOnePoint - $this->playerTwoPoint) >= 2;
         $isPlayerTwoWin = $this->playerTwoPoint >= 4 && $this->playerOnePoint >= 0 && ($this->playerTwoPoint - $this->playerOnePoint) >= 2;
 
-        if ($scoreIsTied) {
-            $score = $this->getScoreType($this->playerOnePoint);
-            $score .= "-All";
-        }
-
-        if ($scoreIsDeuce) {
-            $score = "Deuce";
-        }
-
-        if ($isPlayerOneAdvantage) {
-            $score = "Advantage player1";
-        }
-
-
-        if ($isPlayerTwoAdvantage) {
-            $score = "Advantage player2";
-        }
-
         if ($isPlayerOneWin) {
             return "Win for player1";
         }
 
-
         if ($isPlayerTwoWin) {
             return "Win for player2";
         }
-        if ($this->playerHasPointsAndTheOtherDoesnt($this->playerOneName) || $this->playerHasPointsAndTheOtherDoesnt($this->playerTwoName) || $this->isPlayerWinning($this->playerOneName) || $this->isPlayerWinning($this->playerTwoName)) {
-            $this->playerOneResult = $this->getScoreType($this->playerOnePoint);
-            $this->playerTwoResult = $this->getScoreType($this->playerTwoPoint);
-            $score = "{$this->playerOneResult}-{$this->playerTwoResult}";
+
+        if ($scoreIsDeuce) {
+            return "Deuce";
+        }
+
+        if ($scoreIsTied) {
+            $score = $this->getScoreType($this->playerOnePoint);
+            $score .= "-All";
+            return $score;
+        }
+
+        if ($isPlayerOneAdvantage) {
+            return "Advantage player1";
+        }
+
+        if ($isPlayerTwoAdvantage) {
+            return "Advantage player2";
         }
 
 
-
-        return $score;
+        $this->playerOneResult = $this->getScoreType($this->playerOnePoint);
+        $this->playerTwoResult = $this->getScoreType($this->playerTwoPoint);
+        return "{$this->playerOneResult}-{$this->playerTwoResult}";
     }
-
 
     public function wonPoint($player) : void
     {
